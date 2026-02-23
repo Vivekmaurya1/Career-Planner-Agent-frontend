268076200a93e9e74c07326e1b3c3e3b9c770668
Vivekmaurya1 <vivekmaurya8273@gmail.com>
2026-02-23 17:28:24 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,6 +7,7 @@ const instance = axios.create({
   },
 });
 
+
 /* 🔐 Attach token automatically + check expiry before request */
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
